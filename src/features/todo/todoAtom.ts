4e2878df504ff795558eb6275aedd5af83be9a00
平泉 遥輝 <ha-hiraizumi@kentem.co.jp>
2025-10-19 15:13:32 +0000

    todo form state

---
+++ src/features/todo/todoAtom.ts
@@ -0,0 +1,26 @@
+import { atom, useAtom } from "jotai";
+
+type Todo = {
+  id: string;
+  title: string;
+  completed: boolean;
+  endAt?: Date;
+};
+
+const todoAtom = atom<Todo[]>([]);
+
+export const useTodoAtom = () => {
+  const [todos, setTodos] = useAtom(todoAtom);
+
+  const addTodo = (newTodo: Omit<Todo, "id" | "completed">) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { ...newTodo, id: crypto.randomUUID(), completed: false },
+    ]);
+  };
+  const removeTodo = (id: string) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  return { todos, addTodo, removeTodo };
+};
--- src/features/todo/todoForm.tsx
+++ src/features/todo/todoForm.tsx
@@ -1,27 +1,45 @@
+import { useTodoAtom } from "./todoAtom";
+
 export const TodoForm = () => {
+  const { addTodo } = useTodoAtom();
+  const onSubmit = async (data: FormData) => {
+    const formData = Object.fromEntries(data.entries());
+
+    addTodo({
+      title: formData.title as string,
+      endAt: formData.endAt ? new Date(formData.endAt as string) : undefined,
+    });
+  };
+
   return (
-    <div className="p-4 bg-slate-100 w-120 max-w-[90%] rounded">
+    <form
+      className="p-4 bg-slate-100 w-120 max-w-[90%] rounded"
+      action={onSubmit}
+    >
       <label>
         ToDo Title*
         <input
           type="text"
+          name="title"
           className="p-2 border-2 border-slate-300 rounded w-full"
           placeholder="ToDo名を入力"
+          required
         />
       </label>
       <label>
         ToDo EndAt
         <input
           type="date"
+          name="endAt"
           className="p-2 border-2 border-slate-300 rounded w-full"
         />
       </label>
       <button
-        type="button"
+        type="submit"
         className="mt-4 px-4 py-2 w-full cursor-pointer bg-teal-600 text-white rounded hover:bg-teal-700"
       >
         Add ToDo
       </button>
-    </div>
+    </form>
   );
 };
